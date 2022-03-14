1b2a83be43163abc4ddc54a4020d4426238f8792
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-03-14 13:27:49 +0200

    create home navigation

--- app/src/main/java/com/example/recipeium/MainActivity.kt
+++ app/src/main/java/com/example/recipeium/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.recipeium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.recipeium.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //region  variables
+    private lateinit var navigation: NavController
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    //endregion
+
+    //region events
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        navigation = navHostFragment.findNavController()
+        appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.homeFragment, R.id.favouriteFragment, R.id.jokeFragment))
+
+        binding.bottomNavigationView.setupWithNavController(navigation)
+        setupActionBarWithNavController(navigation, appBarConfiguration)
+
+
     }
+
+    override fun onNavigateUp(): Boolean {
+        return navigation.navigateUp() || super.onNavigateUp()
+    }
+
+    //endregion
+
+    //region helper functions
+
+
+    //endregion
 }
