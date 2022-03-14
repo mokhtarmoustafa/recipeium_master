4e8b399a4615a44734a34b9796c3540272e613a1
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-03-14 22:14:58 +0200

    home- create list item place holder design

--- app/src/main/java/com/example/recipeium/HomeFragment.kt
+++ app/src/main/java/com/example/recipeium/HomeFragment.kt
@@ -5,19 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.recipeium.databinding.FragmentHomeBinding
+import com.example.recipeium.databinding.FragmentHomeBindingImpl
 
 
 class HomeFragment : Fragment() {
+//region variables
 
+    //endregion
+    private lateinit var binding:FragmentHomeBinding
+    //region events
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding=FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+
+    //endregion
+
+    //region helper functions
+    private fun init()
+    {
+        binding.shimmerView.startShimmer()
+    }
+    //endregion
     companion object {
         private const val TAG = "HomeFragment"
     }
