e8afbb5c39f7fce929256be3e22057af286b536d
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-03-15 11:33:24 +0200

    update models

--- app/src/main/java/com/example/recipeium/models/Result.kt
+++ app/src/main/java/com/example/recipeium/models/Result.kt
@@ -6,56 +6,26 @@ import com.google.gson.annotations.SerializedName
 data class Result(
     @SerializedName("aggregateLikes")
     val aggregateLikes: Int,
-    @SerializedName("analyzedInstructions")
-    val analyzedInstructions: List<AnalyzedInstruction>,
     @SerializedName("cheap")
     val cheap: Boolean,
-    @SerializedName("creditsText")
-    val creditsText: String,
-    @SerializedName("cuisines")
-    val cuisines: List<String>,
     @SerializedName("dairyFree")
     val dairyFree: Boolean,
-    @SerializedName("diets")
-    val diets: List<String>,
-    @SerializedName("dishTypes")
-    val dishTypes: List<String>,
-    @SerializedName("gaps")
-    val gaps: String,
+    @SerializedName("extendedIngredients")
+    val extendedIngredients: List<ExtendedIngredient>,
     @SerializedName("glutenFree")
     val glutenFree: Boolean,
-    @SerializedName("healthScore")
-    val healthScore: Double,
     @SerializedName("id")
     val id: Int,
     @SerializedName("image")
     val image: String,
-    @SerializedName("imageType")
-    val imageType: String,
-    @SerializedName("license")
-    val license: String,
-    @SerializedName("lowFodmap")
-    val lowFodmap: Boolean,
-    @SerializedName("occasions")
-    val occasions: List<Any>,
-    @SerializedName("pricePerServing")
-    val pricePerServing: Double,
     @SerializedName("readyInMinutes")
     val readyInMinutes: Int,
-    @SerializedName("servings")
-    val servings: Int,
     @SerializedName("sourceName")
     val sourceName: String,
     @SerializedName("sourceUrl")
     val sourceUrl: String,
-    @SerializedName("spoonacularScore")
-    val spoonacularScore: Double,
-    @SerializedName("spoonacularSourceUrl")
-    val spoonacularSourceUrl: String,
     @SerializedName("summary")
     val summary: String,
-    @SerializedName("sustainable")
-    val sustainable: Boolean,
     @SerializedName("title")
     val title: String,
     @SerializedName("vegan")
@@ -66,6 +36,4 @@ data class Result(
     val veryHealthy: Boolean,
     @SerializedName("veryPopular")
     val veryPopular: Boolean,
-    @SerializedName("weightWatcherSmartPoints")
-    val weightWatcherSmartPoints: Int
 )
