331e143545ab094611b3debc6dbf4a04e528dc98
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-04-11 15:36:11 +0200

    create home

--- app/src/main/java/com/example/recipeium/MainActivity.kt
+++ app/src/main/java/com/example/recipeium/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.recipeium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.recipeium.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //region variables
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    //endregion
+
+    //region events
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+
     }
+
+    //endregion
+
+    //region helper functions
+    private fun init() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.layout.fragment_recipe,
+                R.layout.fragment_favourite2,
+                R.layout.fragment_joke2
+            )
+        )
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    //endregion
 }
